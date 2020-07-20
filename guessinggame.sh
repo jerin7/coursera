5d6ba40610f733ae17ddfceda362822af6389eb9
jerin7 <68042202+jerin7@users.noreply.github.com>
2020-07-20 14:11:41 +0530

    Update guessinggame.sh

--- guessinggame.sh
+++ guessinggame.sh
@@ -2,14 +2,14 @@ function guess(){
     true_ans=$(ls -l |grep "^-"|wc -l)
     while true;
     do
-        echo "pleas enter your guess"
+        echo "please enter your guess"
         read  number
         if [ $number -lt $true_ans ]
         then
-            echo "your guess is Less then the true number"
+            echo "Your guess is less than the true number"
         elif [ $number -gt $true_ans ]
         then
-            echo "your guess is Greater then the true number"
+            echo "Your guess is greater than the true number"
         else
             echo " congratulation,you are right!"
         break;
